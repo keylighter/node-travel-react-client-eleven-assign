6e33d6875da4f58d30cb7148e3b53c7a09096c1e
keylighter <keysuggest@gmail.com>
2021-10-31 23:45:02 +0600

    spinner

--- src/component/Offers/Offers.js
+++ src/component/Offers/Offers.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
+import useAuth from '../../hooks/useAuth';
 import Offer from '../Offer/Offer';
 
 const Offers = () => {
 
+    const { isLoading } = useAuth();
     const [offers, setOffers] = useState([]);
 
     // Heroku - https://blooming-wave-96355.herokuapp.com/
@@ -16,6 +18,15 @@ const Offers = () => {
     return (
         <div className='pb-5'>
             <h2 className='text-center pt-5 pb-5 fw-bold '>Your Tour Options</h2>
+
+            {
+                isLoading &&
+                <div className=' text-center pb-4 pt-5'>
+                    <div className="spinner-border" role="status">
+                        <span className="visually-hidden">Loading...</span>
+                    </div>
+                </div>
+            }
             <div className='row justify-content-center ps-5 ms-5  gy-4 '>
                 {/* mapping services */}
                 {
